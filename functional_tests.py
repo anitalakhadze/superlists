319039d6152920e0aed8635eb21882713a777530
atala15 <atala15@freeuni.edu.ge>
2020-06-03 16:10:59 +0400

    minor improvement

--- functional_tests.py
+++ functional_tests.py
@@ -37,16 +37,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for now in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finishe the test!')
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
