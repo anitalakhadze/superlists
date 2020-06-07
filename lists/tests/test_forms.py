d2ea31dac5da7a314a08da7451acff76660e918f
atala15 <atala15@freeuni.edu.ge>
2020-06-08 01:00:19 +0400

    Used the existing list item form in the list view

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -28,12 +28,12 @@ class ItemFormTest(TestCase):
 class ExistingListItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
-        lits_ = List.objects.create()
+        list_ = List.objects.create()
         form = ExistingListItemForm(for_list=list_)
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
-        lits_ = List.objects.create()
+        list_ = List.objects.create()
         form = ExistingListItemForm(for_list=list_, data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
@@ -43,4 +43,10 @@ class ExistingListItemFormTest(TestCase):
         Item.objects.create(list=list_, text='no twins!')
         form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['text'], DUPLICATE_ITEM_ERROR)
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
